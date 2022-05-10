3d6cdd01d04c234c30560cf991ae7c880b3dc1ea
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-10 17:13:51 +0200

    pusho controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('/pets','PetController');
