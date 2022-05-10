a52c76221f8d43665ed845496d4c0f4167ff6bfc
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-05-10 17:01:26 +0200

    add migration

--- database/migrations/2022_05_10_143050_create_pets_table.php
+++ database/migrations/2022_05_10_143050_create_pets_table.php
@@ -15,6 +15,9 @@ class CreatePetsTable extends Migration
     {
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('species',100);
+            $table->integer('age');
             $table->timestamps();
         });
     }
