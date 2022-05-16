1d0a584ba6a4d4fc8b11a37e33eae39c5a2ee4fd
mikicola <michela.colangelo@gmail.com>
2022-05-16 11:17:20 +0200

    aggiunta stile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('templates.base')
 @section('title', 'home')
 @section('content')
-    <a href="{{ route('pets.index') }}">View Pets Listings</a>
+    <div class="container my-5">
+        <a href="{{ route('pets.index') }}" class="text-decoration-none">View Pets Listings</a>
+    </div>
 @endsection
